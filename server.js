7fa4fa317856b104136049f39f1400a6f4b40d7e
dannohh <dtwomey13@gmail.com>
2021-01-06 15:04:00 -0600

    import and respond to maint request via text

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 const express = require('express')
 const app = express()
-
+const dotenv = require('dotenv')
+dotenv.config()
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static(__dirname + '/public'));
-// app.use('/styles', express.static(__dirname + '/public'))
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
 app.get('/', (req, res) => {
     res.render('index')
     res.end()
 })
 
-
 app.post('/order', (req, res) => {
     console.log('got request')
+    
+    client.messages
+  .create({
+     body: `${req.body.textArea} ${req.body.houseNumber} ${req.body.streetName}`,
+     from: '+14696096299',
+     to: '+16362344679'
+   })
+  .then(message => console.log(message.sid))
+
     res.send(`<div 
     style="display: flex; 
     flex-direction: column; 
@@ -36,6 +47,7 @@ app.post('/order', (req, res) => {
     </div>`)
     console.log(req.body)
 
+
     res.end()
 })
 app.get('/order', (req, res) => {
@@ -43,4 +55,4 @@ app.get('/order', (req, res) => {
     res.end()
 })
 
-app.listen(process.env.PORT, console.log('listening on PORT...'))
+app.listen(3000 || process.env.PORT, console.log('listening on PORT...'))
