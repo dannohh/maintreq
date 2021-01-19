49932d71a6af3780cc1302cdca2bfe6bd3162a10
dannohh <dtwomey13@gmail.com>
2021-01-18 18:47:10 -0600

    update form

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = require('twilio')(accountSid, authToken);
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -53,4 +53,4 @@ app.get('/order', (req, res) => {
     res.end()
 })
 
-app.listen(process.env.PORT, console.log('listening on PORT...'))
+app.listen(3000, console.log('listening on PORT...'))
